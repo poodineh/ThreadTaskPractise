d02ca9ba69a203b8eb331ca48f1b9f0cc49107f5
Mahdi Poodineh <mahdi.poodineh@eci-m.com>
2022-02-04 15:19:09 +0100

    Test 3

---
+++ ThreadTestProj/ThreadTestProj3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTestProj3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new class1();
+            class1.RunTask();
+            class1.task2.Start();
+            Console.ReadKey();
+        }
+    }
+
+    public class class1
+    {
+        Task task1 = new Task(() => {
+            Console.WriteLine("task1");
+            Thread.Sleep(1000);
+            });
+
+        public Task task2 = new Task(() => Console.WriteLine("task2"));
+        public void RunTask()
+        {
+            task1.Start();
+        }
+    }
+}
