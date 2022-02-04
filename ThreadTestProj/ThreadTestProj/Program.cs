d459f5bc40b3dbab2060ebed4a8b6de955702897
Mahdi Poodineh <mahdi.poodineh@eci-m.com>
2022-02-04 11:38:04 +0100

    Add project files.

---
+++ ThreadTestProj/ThreadTestProj/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread1 = new Thread(Thread1Job);
+            var thread2 = new Thread(Thread2Job);
+            var thread3 = new Thread(Thread3Job);
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+        private static void Thread3Job()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " from thread3:" + i);
+            }
+        }
+
+        private static void Thread2Job()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " from thread2:" + i);
+            }
+        }
+
+        private static void Thread1Job()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " from thread1:" + i);
+            }
+        }
+    }
+}
---
+++ ThreadTestProj/ThreadTestProj2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTestProj2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main threadId: " + Thread.CurrentThread.ManagedThreadId);
+
+            Task task = Task.Run(() => Add(2,6));
+            Task<int> task2 = new Task<int>(()=>Add(2,6));
+            task2.Start();
+            Console.WriteLine("Task in Main method.");
+            Console.WriteLine("Task2 : ");
+            Console.WriteLine(task2.Result);
+            Console.ReadKey();
+        }
+
+        private static int Add(int n1, int n2)
+        {
+            int x = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                x++;
+                Thread.Sleep(100);
+                Console.WriteLine(x + " -> Add threadId: " + Thread.CurrentThread.ManagedThreadId);
+            }
+            return n1 + n2;
+        }
+    }
+}
